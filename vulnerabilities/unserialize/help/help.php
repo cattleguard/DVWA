d7b3115e54b4f9fc40bff6389467a8ed000042f6
Anthony <anthonytowry@gmail.com>
2016-08-31 21:40:50 -0500

    php object injection help

--- vulnerabilities/unserialize/help/help.php
+++ vulnerabilities/unserialize/help/help.php
@@ -5,6 +5,17 @@
 	<table width='100%' bgcolor='white' style="border:2px #C0C0C0 solid">
 	<tr>
 	<td><div id="code">
+		<h3>About</h3>
+		<p>
+			PHP Object Injection is a server-side vulnerability class in which an attacker is 
+			able to arbitrarily instantiate an object of a type (class) previously loaded by the application.  
+			This can result in code execution, file overwrite, etc.
+		</p>
+		<p>
+			This occurs by way of using the unserialize() method to load an user controlled serialized object.
+			I know what you're thinking.  So I can create an object, how does that get me code execution?  Well,
+			PHP classes have a set of "magic methods".
+		</p>
 	</div></td>
 	</tr>
 	</table>
@@ -14,4 +25,5 @@
 	<br />
 
 	<p>Reference: <?php echo dvwaExternalLinkUrlGet( 'https://www.owasp.org/index.php/PHP_Object_Injection' ); ?></p>
+	<p>Reference: <?php echo dvwaExternalLinkUrlGet( 'https://www.insomniasec.com/downloads/publications/Practical%20PHP%20Object%20Injection.pdf' ); ?></p>
 </div>
